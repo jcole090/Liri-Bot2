1dad8ec4f48bd1cb098409291d4132310534fbac
Jared Cole-Lewis <jaredcole-lewis@Jareds-Air.fios-router.home>
2019-05-18 13:25:38 -0400

    Created basic band and movie requests.

--- liri.js
+++ liri.js
@@ -2,7 +2,31 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+var axios = require('axios');
 
-"node liri.js concert-this <artist/band name here>"
-"https://rest.bandsintown.com/artists/" + ar
+//var spotify = new Spotify(keys.spotify);
+
+
+
+const movieRequest = title => axios.get(`http://www.omdbapi.com/?apikey=${process.env.OMDB_KEY}&t=${title}`)
+.then(function (response) {
+  console.log(response);
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+//movieRequest("blade")
+
+const bandRequest = artist => axios.get(`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`)
+.then(function (response) {
+  console.log(response);
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+bandRequest("beatles") 
+
+//"node liri.js concert-this <artist/band name here>"
+//"https://rest.bandsintown.com/artists/" 
